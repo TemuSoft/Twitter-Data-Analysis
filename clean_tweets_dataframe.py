10752eb586ad6057a8fea479f8573b5664d61a4e
Temesgen Kefie <temusoft2012@gmail.com>
2022-08-09 21:59:12 +0300

    Fix bugs inside clean tweets data frame and remobve unused .gitignore names

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -22,13 +22,13 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
+        df.drop_duplicates()
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
@@ -39,10 +39,8 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd
-        
-        
-        
+        df.columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
+       
         return df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
@@ -50,6 +48,6 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = pd
+        df.lang.str.replace('[^a-zA-Z0-9]', '')
         
         return df
